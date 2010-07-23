fce9661dd0139b8db5387068be523c2b79655e79
Ben Woosley <ben.woosley@gmail.com>
2010-07-23 13:23:13 -0400

    #autosizer_attachment is more flexible...

--- lib/paperclip_autosizer.rb
+++ lib/paperclip_autosizer.rb
@@ -4,8 +4,8 @@ class PaperclipAutosizer < ActiveRecord::Base
   attr_writer :autosizer_attachment_name, :autosizer_original_file_geometry
 
 private
-  def autosizer_attachment_styles
-    send(@autosizer_attachment_name).styles
+  def autosizer_attachment
+    send(@autosizer_attachment_name)
   end
 
   def autosize_attached_files
@@ -15,7 +15,7 @@ private
   end
 
   def styles_to_autosize
-    styles = autosizer_attachment_styles.keys
+    styles = autosizer_attachment.styles.keys
     styles.inject({}) do |accumulator, style|
       column_for_style = [@autosizer_attachment_name, style.to_s, "size"].join("_")
       if self.class.column_names.include?(column_for_style)
@@ -26,7 +26,7 @@ private
   end
 
   def calculate_size_of_reduced_image(style)
-    target_width, target_height = autosizer_attachment_styles[style][:geometry].split("x").collect{|x| x.to_f}
+    target_width, target_height = autosizer_attachment.styles[style][:geometry].split("x").collect{|x| x.to_f}
     width, height = @autosizer_original_file_geometry.width, @autosizer_original_file_geometry.height
     original_ratio = width.to_f / height.to_f
     if (original_ratio <= 1 && target_height < height)
