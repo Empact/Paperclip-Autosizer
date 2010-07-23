8a9041e02af845e59d16f746230fa8d44998d965
Ben Woosley <ben.woosley@gmail.com>
2010-07-23 13:16:17 -0400

    Extract autosizer_attachment_styles

--- lib/paperclip_autosizer.rb
+++ lib/paperclip_autosizer.rb
@@ -4,6 +4,10 @@ class PaperclipAutosizer < ActiveRecord::Base
   attr_writer :autosizer_attachment_name, :autosizer_original_file_geometry
 
 private
+  def autosizer_attachment_styles
+    self.send(@autosizer_attachment_name).styles
+  end
+
   def autosize_attached_files
     styles_to_autosize.each_pair do |style, column_for_style|
       target_size = calculate_size_of_reduced_image(style)
@@ -12,7 +16,7 @@ private
   end
 
   def styles_to_autosize
-    styles = self.send(@autosizer_attachment_name).styles.keys
+    styles = autosizer_attachment_styles.keys
     styles.inject({}) do |accumulator, style|
       column_for_style = [@autosizer_attachment_name, style.to_s, "size"].join("_")
       if self.class.column_names.include?(column_for_style)
@@ -23,7 +27,7 @@ private
   end
 
   def calculate_size_of_reduced_image(style)
-    target_width, target_height = self.send(@autosizer_attachment_name).styles[style][:geometry].split("x").collect{|x| x.to_f}
+    target_width, target_height = autosizer_attachment_styles[style][:geometry].split("x").collect{|x| x.to_f}
     width, height = @autosizer_original_file_geometry.width, @autosizer_original_file_geometry.height
     original_ratio = width.to_f / height.to_f
     if (original_ratio <= 1 && target_height < height)
